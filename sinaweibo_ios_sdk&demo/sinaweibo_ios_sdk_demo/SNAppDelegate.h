e555707e88bb9c2a830e8b9deb369d770219c126
bitpanda <panda@bit.local>
2012-12-19 13:52:10 +0800

    更新
    更新目录结构，代码未变。

--- sinaweibo_ios_sdk&demo/sinaweibo_ios_sdk_demo/SNAppDelegate.h
+++ sinaweibo_ios_sdk&demo/sinaweibo_ios_sdk_demo/SNAppDelegate.h
@@ -8,6 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
+/**将下面注释取消，并定义自己的app key，app secret以及授权跳转地址uri
+ 此demo即可编译运行**/
+
 //#define kAppKey             @"your app_key"
 //#define kAppSecret          @"your app_secret"
 //#define kAppRedirectURI     @"your app_rederict_uri"
