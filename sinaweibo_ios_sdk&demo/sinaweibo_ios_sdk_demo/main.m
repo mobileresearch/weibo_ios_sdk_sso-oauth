cc9259a1385ff2e1f961f49ecd30a2dc869c61c1
mobileresearch <sinamobilemt@gmail.com>
2013-05-22 19:42:51 +0800

    回

---
+++ sinaweibo_ios_sdk&demo/sinaweibo_ios_sdk_demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  sinaweibo_ios_sdk_demo
+//
+//  Created by Wade Cheng on 4/23/12.
+//  Copyright (c) 2012 SINA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SNAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SNAppDelegate class]));
+    }
+}
